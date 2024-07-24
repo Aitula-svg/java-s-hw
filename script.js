ffda38689702d7be56d72817abe4738f63e3ea5a
Aitula-svg <aitu.ru00@gmail.com>
2024-07-24 18:31:18 +0300

    project

---
+++ script.js
@@ -0,0 +1,119 @@
+
+// Задание 1
+
+// let counter = 1
+
+
+// while(counter < 101) {
+//     console.log(counter);
+//     counter= counter + 1;
+// }
+
+// Задание 2
+
+// let counter = 2;
+// while(counter <= 100) {
+//     if(counter % 2 === 0){
+//     console.log(counter);
+// }
+// counter++
+// }
+
+
+// Задание 3
+
+// let counter = 0;
+//  let sum = 0;
+
+//  while (counter <= 100) {
+//     sum = sum + counter;
+//     counter++
+//  }
+
+//  console.log(sum);
+
+// Задание 4
+
+// let searchingChar = prompt("Enter your char")
+
+// let userName = "Naruto Shipuden"
+
+// let counter = 0;
+
+// while (counter < userName.length) {
+//     console.log(userName.charAt(11));
+
+//     counter++;
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// Задание 5
+
+// let drink = "Черноловка"
+
+// let counter = drink.length;
+
+// while(counter >= 0) {
+//     console.log(drink.charAt(counter));
+
+//     counter--
+// }
+
+
+
+
+
+// Задание 7
+
+
+// var x, y, chr;
+
+// for (x = 1; x <= 8  ; x++) {
+   
+//     for (y = 1; y < x; y++) {
+        
+//         chr = chr + ("#");
+//     }
+
+  
+//     console.log(chr);
+
+  
+//     chr = '';
+// } 
+
+
+
+
+
+// var x, y, chr;
+
+// for (x = 1; x <= 8  ; x++) {
+   
+//     for (y = 1; y < x; y++) {
+        
+//         chr = chr + ("#");
+//     }
+
+  
+//     console.log(chr);
+
+  
+//     chr = '';
+// } 
+
+
+
+
+
